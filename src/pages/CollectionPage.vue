fe267b0cef0cf27beda0e7fa7928280b96006416
Ferdinand-Snapa <ferdinand@snapa.no>
2026-04-13 17:22:53 +0200

    Contact page with image

--- src/pages/CollectionPage.vue
+++ src/pages/CollectionPage.vue
@@ -29,7 +29,7 @@
 </script>
 
 <template>
-    <div class="w-screen bg-opacity-50 mt-[10vh]">
+    <div class="w-screen bg-opacity-50 mt-[12vh]">
         <div class="columns-1 sm:columns-3 lg:columns-4 xl:columns-5 gap-4 mx-24 lg:mx-28 xl:mx-32">
             <div class="h-20 mb-4 flex bg-secondary dark:bg-dark-secondary rounded-xl">
                 <img v-if="data.filterSoft"
@@ -51,9 +51,17 @@
                 </div>
                 
             </div>
-            <div v-if="filteredProjects[0] === undefined">
-                Oops either someone lying on portfolio
-                or havn't gotten around to adding project using {{ getSoftware([filterSoftId])[0]?.name }}
+            <div v-if="filteredProjects[0] === undefined" class="text-gray-600 dark:text-background">
+                <div v-if="data.prefLang ==='no'">
+                    Ups, enten lyver noen på porteføljen,
+                    eller så har de ikke fått lagt til prosjektet ved hjelp av {{ getSoftware([select])[0]?.name }}
+
+                </div>
+                <div v-else>
+                    Oops either someone lying on portfolio
+                    or havn't gotten around to adding project using {{ getSoftware([select])[0]?.name }}
+                </div>
+                
             </div>
             <div
                 v-else
--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,3 +1,44 @@
+<script setup>
+import FerdImg from '../assets/img/_DSC4963.JPG'
+
+</script>
+
 <template>
-    <div>This is contact me page</div>
+    <div class="mt-28 mx-20 flex flex-col md:flex-row gap-8">
+        <!-- Image Section -->
+        <div class="w-full md:w-1/2 flex items-center justify-center">
+            <img :src="FerdImg" class="w-full h-auto object-cover rounded-lg">
+        </div>
+
+        <!-- Contact Information Section -->
+        <div class="w-full md:w-1/2 flex flex-col justify-center items-center md:items-start gap-6">
+            
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:flex md:flex-col gap-4 w-full text-md md:text-xl">
+                <div class="flex flex-col items-center md:items-start">
+                    <h2 class="text-xl font-semibold">Email</h2>
+                    <p class="text-gray-600 dark:text-background">ferdinand@snapa.no</p>
+                </div>
+
+                <div class="flex flex-col items-center md:items-start">
+                    <h2 class="text-xl font-semibold">Phone</h2>
+                    <p class="text-gray-600 dark:text-background">+47 916 32 442</p>
+                </div>
+
+                <div class="flex flex-col items-center md:items-start">
+                    <h2 class="text-xl font-semibold">Location</h2>
+                    <p class="text-gray-600 dark:text-background">Sørumsand, Akerhus</p>
+                    <p class="text-gray-600 dark:text-background">15 min → Lillestrøm</p>
+                    <p class="text-gray-600 dark:text-background">30 min → Oslo S</p>
+                </div>
+
+                <div class="flex flex-col items-center md:items-start">
+                    <h2 class="text-xl font-semibold">Social Media</h2>
+                    <div class="flex gap-4">
+                        <a href="https://www.linkedin.com/in/ferdinand-snapa-a61576285/" class="text-blue-500 hover:text-blue-700">LinkedIn</a>
+                        <a href="https://github.com/Ferdinand-Snapa" class="text-blue-500 hover:text-blue-700">GitHub</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
